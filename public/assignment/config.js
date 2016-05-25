291c3270c234620d4318b874bd9ef030471be170
mreko <mreko@ccs.neu.edu>
2016-05-25 11:45:41 -0400

    work on websites pages and linkage

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -17,6 +17,15 @@
                 templateUrl: "views/user/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model"
+            })
+            .when("/website-list", {
+                templateUrl: "views/website/website-list.view.client.html"
+            })
+            .when("/website-edit", {
+                templateUrl: "views/website/website-edit.view.client.html"
+            })
+            .when("/website-new", {
+                templateUrl: "views/website/website-new.view.client.html"
             });
     }
 })();
